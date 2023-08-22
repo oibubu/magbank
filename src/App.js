423d14f429261b3c58fd38786fa81ba14b418eac
Bruno Vilhena <vilhenabruno0@gmail.com>
2023-08-22 20:45:35 -0300

    FAQ

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
-import Cartao from "./components/Cartao"
+import CreditCard from './components/CreditCard';
+import CardList from './components/CardList';
+import CenteredButton from './components/CenteredButton';
+import Institutional from './components/Institutional';
+
 import "./App.scss";
+
+import posts from './data/posts';
 
 const App = () => (
     <div className="App">
       <Navbar/>
       <Hero/>
-      <Cartao/>
+
+      <CreditCard />
+      <CardList posts={posts} />
+      <CenteredButton>Abra sua conta</CenteredButton>
+
+      <Institutional />
     </div>
   );
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,14 +8,14 @@ import LogoDesktop from '../assets/paycopy--Desktop.png';
 const Hero = () => (
     <div class='jumbotron text-center text-light hero'>
         <Row className='my-lg-5'>
-            <Col lg className="text-lg-end my-lg-5">
-                <Image className="d-lg-none" src={Logo} />
-                <Image className="d-none d-lg-inline-block" src={LogoDesktop} />
-            </Col>
-            <Col lg className='text-lg-start my-lg-5'>
-                <p>Pague suas contas pelo nosso APP</p>
-                <Button variant="outline-ligth">Abra sua conta</Button>
-            </Col>
+        <Col lg className='text-lg-right my-lg-5'>
+            <Image className='d-lg-none' src={Logo} />
+            <Image className='d-none d-lg-inline-block' src={LogoDesktop} />
+        </Col>
+        <Col lg className='text-lg-left my-lg-5'>
+            <p>Pague suas contas pelo nosso APP</p>
+            <Button variant='outline-light'>Abra sua conta</Button>
+        </Col>
         </Row>
     </div>
 );
