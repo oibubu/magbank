006be3aa17bf4c6a2707a1564f11191f6329ed46
Bruno Vilhena <vilhenabruno0@gmail.com>
2023-08-23 20:28:21 -0300

    bug fix

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import CreditCard from './components/CreditCard';
 import CardList from './components/CardList';
 import CenteredButton from './components/CenteredButton';
 import Institutional from './components/Institutional';
+import Faq from './components/Faq'
 
 import "./App.scss";
 
 import posts from './data/posts';
 
 const App = () => (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
+  <div className="App">
+    <Navbar />
+    <Hero />
 
-      <CreditCard />
-      <CardList posts={posts} />
-      <CenteredButton>Abra sua conta</CenteredButton>
+    <CreditCard />
+    <CardList posts={posts} />
+    <CenteredButton>Abra sua conta</CenteredButton>
 
-      <Institutional />
-    </div>
-  );
+    <Institutional />
+    <Faq/>
+  </div>
+);
 
 export default App;
